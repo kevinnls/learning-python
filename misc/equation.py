21bd28b0daf8e151b7e5bd8636f290b0447641cf
kevinnls <kevin.nl.samuel@gmail.com>
2020-04-21 21:53:42 +0530

    rearranged

---
+++ misc/equation.py
@@ -0,0 +1,10 @@
+result = 0.0
+x = 0.0
+while result < 20.00:
+	#x = 1
+	result = 1.0 + x + (x**2) + (x**3)
+	x += 0.1
+#print(str(result) + str(x))
+print(str(x) + "\t" + str(result))
+
+print (1.0 + 2.6427 + (2.6427**2) + (2.6427**3))
