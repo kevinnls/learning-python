4da28ee9cf2e28409853ddc769b2a98eb561ecb5
kevinnls <kevin.nl.samuel@gmail.com>
2020-04-23 20:51:32 +0530

    improved ex_4.py completed ex_5.py

--- LearningHardWay/20200423/ex_4.py
+++ LearningHardWay/20200423/ex_4.py
@@ -1,11 +1,11 @@
 total_cars = 345
-cars_thrashed = {"the hulk":50,"superman":67,"batman":150}
+cars_damaged = {"the hulk":{"thrashed":50},"superman":{"destroyed":67},"batman":{"has blasted":150}}
 
 print("Total Cars\t:",total_cars)
-print("Thrashed")
-for i in cars_thrashed:
-    print("By",i.title()+"\t:",cars_thrashed[i])
-sum_thrashed=0
-for i in cars_thrashed.values():
-    sum_thrashed+=int(i)
-print("Cars remaining\t:",total_cars-sum_thrashed)
+print("\nDamaged")
+sum_damaged=0
+for person in cars_damaged:
+    for action in cars_damaged[person]:
+        print(person.title(),action,"\t:",cars_damaged[person][action])
+        sum_damaged+=cars_damaged[person][action]
+print("\nCars remaining\t:",total_cars-sum_damaged)
---
+++ LearningHardWay/20200423/ex_5.py
@@ -0,0 +1,12 @@
+name = "Kevin"
+age = 19
+height = "something less than 6'"
+weight = "idk somewhat around 70kgs?"
+where = "at home"
+why = "because of the lockdown"
+when = "for the past couple of months"
+
+intro = [name,age,height,weight]
+for detail in intro:
+    print(f"I am {detail}")
+print(f"I am {where} {why} {when}")
