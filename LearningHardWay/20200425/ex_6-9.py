d2daf7966cea7158a5c749069919b5053e84e0e9
kevinnls <kevin.nl.samuel@gmail.com>
2020-04-25 21:09:36 +0530

    complete ex_6-9.py

--- LearningHardWay/20200425/ex_6-9.py
+++ LearningHardWay/20200425/ex_6-9.py
@@ -1,7 +1,21 @@
-age = 0o10
-print("Hello!\nMy name is {} and I am 0b{:x} years old".format("K",age))
-c = ['c','h','e','e','s','e']
-print(c[0]+c[1]+c[2]+c[3]+c[4]+c[5],end=' ')
-print("burger")
-print(c[0]+c[1]+c[2]+c[3]+c[4]+c[5],end='\t')
-print("burger")
+output = """
+{start} supposed to be a {p}.
+{start} pretty useless, and annoying to type.
+{start} line 2 of the {p}.
+{start} is something @xeldrago wants us to do.
+\tTo write a {p} and use {strf} in writing the {p}
+
+{start} is pointless.
+The concept of {strf} is not very useful.
+At least not the {strf} function of the class str.
+{start} because there is a need to repetitively define the format by using mutliple braces.
+And then to add to the pain, one needs to define the value of each brace.
+They really do not make anything easier for anyone. I have never seen any programmer need to type such a long {p}.
+But returning to the topic. {start} is supposed to be a {p} about Covid-19.
+
+{start} all that needs to be said about it.
+
+S T A Y
+H O M E
+""".format(start="This is",p="paragraph",strf="string formatting")
+print(output)
