c60392bcad63152a913f5f633eb6a3ce9dc11d64
kevinnls <kevin.nl.samuel@gmail.com>
2020-05-10 23:08:45 +0530

    trials

---
+++ misc/trying escape-b.py
@@ -0,0 +1,23 @@
+print("hello world")
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+        
+obj = _GetchUnix
+val = '\0'
+while val!='\n':
+    val = obj.__call__(obj)
+    print('*',end='')
